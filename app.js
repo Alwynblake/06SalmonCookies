03616a42c0e2b68141cd1f4c15de101d2310ce52
Kent Ketter <kent.ketter1@Gmail.com>
2018-10-29 14:04:16 -0700

    First_Updates

--- app.js
+++ app.js
@@ -1,59 +1,10 @@
 //What is a JS Object?
-//var name = 'Michaelangelo';
 
-function sayHello(name){
-  return `Hello, ${name}, how are you today?`;
+var store1standPike = {
+  location : '1st and Pike',
+  customersPerHour: Math.random,
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
 }
-
-var turtleMichaelangelo = {
-  //property: value,
-  name: 'Michaelangelo',
-  shell: true,
-  age: 15,
-  color: 'orange',
-  type: 'box',
-  size: 62.3,
-  ninja: true,
-  mutant: true,
-  sayHello: function() {
-    return `Hello, my name is ${this.name}`;
-  },
-};
-
-var turtleDonatello = {
-  //property: value,
-  name: 'Donatello',
-  shell: true,
-  age: 15,
-  color: 'purple',
-  type: 'box',
-  size: 62.3,
-  ninja: true,
-  mutant: true,
-  sayHello: function() {
-    return `Hello, my name is ${this.name}`;
-  },
-  render() {
-    //create an element
-    //provide content for that element
-    //append the elemnt to the page (in a specific place)
-    var container = document.createElement('section');
-    var nameEl = document.createElement('h3');
-    var messageEl = document.createElement('p');
-
-    nameEl.textContent = this.name;
-    messageEl.textContent = `I am a ninja and I am ${this.color}`;
-
-
-    container.appendChild(nameEl);
-    container.appendChild(messageEl);  
-    console.log('container', container);
-
-    var mainEl = document.getElementById('main-content');
-    mainEl.appendChild(container);  
-  }
-};
-
-var nums = [1, 2, 3, 4]; // arrays should generally have like data types
-
-turtleMichaelangelo.render;
+;
