e94225547043e977438be2cda86172099ec60000
Kent Ketter <kent.ketter1@Gmail.com>
2018-10-30 17:52:40 -0700

    day1finalsolution

--- app.js
+++ app.js
@@ -1,34 +1,219 @@
-//What is a JS Object?
-var store1standPike = {
-  location : '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCookieSale: 6.3,
-  openHours: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
-  avgCustomersPerHourArray: [],
-  avgCustomersPerHourGenerator: function() {
-    return Math.floor((Math.random() * (this.maxCust - this.minCust+1)) + this.minCust);
-  },
-  totalCookies: function() {
-    var sum = 0;
-    for(var hours = 0; hours < 15; hours++);
-    var totalCookies = Math.floor(this.avgCookieSale * this.avgCustomersPerHourArray());
-    console.log('Cookies', totalCookies);
-    sum = sum + totalCookies;
-  },
-  
+'use strict';
 
-/*
-  counter for length of empty arrary: for(var i = 0; i < avgCustomersPerHourArray.length; i++) { then assign a number to each fuck
+var pikeAndFirst = {
+  name: 'First and Pike',
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookiesPerCust: 6.3,
+  custPerHour: [],
+  cookiesPerHour: [],
+  hoursOfOps: ['6a','7a','8a','9a','10a','11a','12p','1p','2p','3p','4p','5p','6p','7p','8p'],
+  dailyTotal: 0,
+  generateRandomCustPerHour: function() {
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var randomCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+      this.custPerHour.push(randomCust);
+    }
+    console.log('custPerHour', this.custPerHour);
+  },
+  generateHourlySales: function() {
+    // line below will populate custPerHour array
+    this.generateRandomCustPerHour();
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var perHour = Math.round(this.custPerHour[i] * this.avgCookiesPerCust);
+      this.cookiesPerHour.push(perHour);
+      //this.dailyTotal = this.dailyTotal + perHour;
+      this.dailyTotal += perHour;
+    }
+    console.log('cookiesPerHour', this.cookiesPerHour);
+  },
+  render: function() {
+    this.generateHourlySales();
+    // create
+    var container = document.getElementById('main-content');
+    var title = document.createElement('h3');
+    container.appendChild(title);
+    var total = document.createElement('li');
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var hourly = document.createElement('li');
+      hourly.textContent = `${this.hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
+      // ${this.dailyTotal} is in this function, i need to apply this on line 50?
+      container.appendChild(hourly);
+    }
+    // provide content
+    title.textContent = this.name;
+    total.textContent = `Total: ${this.dailyTotal} cookies`;
+
+    // append
+    container.appendChild(total);
+  }
 };
 
-dailyCookiesSold: function () {
-    return this.avgCustomersPerHourArray * this.avgCookieSale;
-  },
+pikeAndFirst.render();
 
-customersPerHour: function getRandomIntInclusive(minCust, maxCust) {
-  minCust = Math.ceil(minCust);
-  maxCust = Math.floor(maxCust);
-  return Math.floor(Math.random() * (maxCust - minCust+1)) + minCust;
-*/
+//start new shop
+
+var seaTacAirport = {
+  name: 'SeaTac Airport',
+  minCustPerHour: 3,
+  maxCustPerHour: 24,
+  avgCookiesPerCust: 1.2,
+  custPerHour: [],
+  cookiesPerHour: [],
+  hoursOfOps: ['6a','7a','8a','9a','10a','11a','12p','1p','2p','3p','4p','5p','6p','7p','8p'],
+  dailyTotal: 0,
+  generateRandomCustPerHour: function() {
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var randomCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+      this.custPerHour.push(randomCust);
+    }
+    console.log('custPerHour', this.custPerHour);
+  },
+  generateHourlySales: function() {
+    // line below will populate custPerHour array
+    this.generateRandomCustPerHour();
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var perHour = Math.round(this.custPerHour[i] * this.avgCookiesPerCust);
+      this.cookiesPerHour.push(perHour);
+      //this.dailyTotal = this.dailyTotal + perHour;
+      this.dailyTotal += perHour;
+    }
+    console.log('cookiesPerHour', this.cookiesPerHour);
+  },
+  render: function() {
+    this.generateHourlySales();
+    // create
+    var container = document.getElementById('main-content');
+    var title = document.createElement('h3');
+    container.appendChild(title);
+    var total = document.createElement('li');
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var hourly = document.createElement('li');
+      hourly.textContent = `${this.hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
+      // ${this.dailyTotal} is in this function, i need to apply this on line 50?
+      container.appendChild(hourly);
+    }
+    // provide content
+    title.textContent = this.name;
+    total.textContent = `Total: ${this.dailyTotal} cookies`;
+
+    // append
+    container.appendChild(total);
+  }
 };
+
+seaTacAirport.render();
+
+//start new shop
+
+var seattleCenter = {
+  name: 'Seattle Center',
+  minCustPerHour: 11,
+  maxCustPerHour: 38,
+  avgCookiesPerCust: 3.7,
+  custPerHour: [],
+  cookiesPerHour: [],
+  hoursOfOps: ['6a','7a','8a','9a','10a','11a','12p','1p','2p','3p','4p','5p','6p','7p','8p'],
+  dailyTotal: 0,
+  generateRandomCustPerHour: function() {
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var randomCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+      this.custPerHour.push(randomCust);
+    }
+    console.log('custPerHour', this.custPerHour);
+  },
+  generateHourlySales: function() {
+    // line below will populate custPerHour array
+    this.generateRandomCustPerHour();
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var perHour = Math.round(this.custPerHour[i] * this.avgCookiesPerCust);
+      this.cookiesPerHour.push(perHour);
+      //this.dailyTotal = this.dailyTotal + perHour;
+      this.dailyTotal += perHour;
+    }
+    console.log('cookiesPerHour', this.cookiesPerHour);
+  },
+  render: function() {
+    this.generateHourlySales();
+    // create
+    var container = document.getElementById('main-content');
+    var title = document.createElement('h3');
+    container.appendChild(title);
+    var total = document.createElement('li');
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var hourly = document.createElement('li');
+      hourly.textContent = `${this.hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
+      // ${this.dailyTotal} is in this function, i need to apply this on line 50?
+      container.appendChild(hourly);
+    }
+    // provide content
+    title.textContent = this.name;
+    total.textContent = `Total: ${this.dailyTotal} cookies`;
+
+    // append
+    container.appendChild(total);
+  }
+};
+
+seattleCenter.render();
+
+//start new shop
+
+var capitolHill = {
+  name: 'Capitol Hill',
+  minCustPerHour: 20,
+  maxCustPerHour: 38,
+  avgCookiesPerCust: 2.3,
+  custPerHour: [],
+  cookiesPerHour: [],
+  hoursOfOps: ['6a','7a','8a','9a','10a','11a','12p','1p','2p','3p','4p','5p','6p','7p','8p'],
+  dailyTotal: 0,
+  generateRandomCustPerHour: function() {
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var randomCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+      this.custPerHour.push(randomCust);
+    }
+    console.log('custPerHour', this.custPerHour);
+  },
+  generateHourlySales: function() {
+    // line below will populate custPerHour array
+    this.generateRandomCustPerHour();
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var perHour = Math.round(this.custPerHour[i] * this.avgCookiesPerCust);
+      this.cookiesPerHour.push(perHour);
+      //this.dailyTotal = this.dailyTotal + perHour;
+      this.dailyTotal += perHour;
+    }
+    console.log('cookiesPerHour', this.cookiesPerHour);
+  },
+  render: function() {
+    this.generateHourlySales();
+    // create
+    var container = document.getElementById('main-content');
+    var title = document.createElement('h3');
+    container.appendChild(title);
+    var total = document.createElement('li');
+
+    for(var i = 0; i < this.hoursOfOps.length; i++) {
+      var hourly = document.createElement('li');
+      hourly.textContent = `${this.hoursOfOps[i]}: ${this.cookiesPerHour[i]} cookies`;
+      // ${this.dailyTotal} is in this function, i need to apply this on line 50?
+      container.appendChild(hourly);
+    }
+    // provide content
+    title.textContent = this.name;
+    total.textContent = `Total: ${this.dailyTotal} cookies`;
+
+    // append
+    container.appendChild(total);
+  }
+};
+
+capitolHill.render();
